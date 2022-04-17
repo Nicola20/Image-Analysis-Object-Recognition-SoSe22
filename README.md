da7847f462f0f64ab2fb78e092e33f9c7efe6ff8
xiqe4652 <nicola.lea.libera@uni-weimar.de>
2022-04-17 19:06:43 +0200

    init setup

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Image-Analysis-Object-Recognition-SoSe22
+
+### Setup on Linux Systems
+- Create virtual environment `python3 -m venv venv`
+- Activate environment `source venv/bin/activate`
+- Install required dependencies `pip install - requirements.txt`
+- If you need more libraries that install them with pip while the virtual environment is activated.
+  Save the new dependencies in the requirements.txt file by running `pip freeze > requirements.txt`
---
+++ assignment1/assignment1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+--------------------------------------------------------------------------------------------
+@Authors: Nicola Lea Libera (117073), Jenny Döring (), Josephin Kröger ()
+Description: Source code to Assignment 1 of the course Image Analysis and Object Recognition
+             at Bauhaus-Universität Weimar.
+--------------------------------------------------------------------------------------------
+"""
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+import argparse
+
+
+def image_enhancement():
+    # implement here task 1
+
+def  binarization():
+    # implement task 2
+
+def morpholigical_operators():
+    # implement task 3 here
+
+def main():
+    # Main function
+
+if __name__ == '__main__':
+    main()
