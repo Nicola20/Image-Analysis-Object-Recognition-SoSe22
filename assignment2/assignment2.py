f3f5ffd7cee90e34a8b69575cc3996cee6b9b05e
xiqe4652 <nicola.lea.libera@uni-weimar.de>
2022-05-10 18:45:19 +0200

    small changes but roundness not fixed

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -14,7 +14,8 @@ from matplotlib import pyplot as plt
 
 SIGMA = 0.5
 
-#loading of the image and converting to gray
+
+# loading of the image and converting to gray
 def load_image(path):
     # reads image as colour image
     img = cv2.imread(path, cv2.IMREAD_COLOR)
@@ -22,13 +23,14 @@ def load_image(path):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     return img
 
-#computation of the kernel
+
+# computation of the kernel
 def create_gaussian_kernel():
-    #5x5 matrix 
+    # 5x5 matrix
     g_x = np.zeros((5, 5), dtype=np.float32)
     # compute the radius of the kernel
     radius = int(np.ceil(3 * SIGMA))
-    #math formula applied and values stored in the matrix
+    # math formula applied and values stored in the matrix
     for y in range(-radius, radius + 1):
         for x in range(-radius, radius + 1):
             tmp1 = - (x / (2 * np.pi * SIGMA ** 4))
@@ -39,74 +41,57 @@ def create_gaussian_kernel():
 
     return g_x, g_y
 
-#computation of magnitude according to formula
+
+# computation of magnitude according to formula
 def compute_magnitude(gradient_x, gradient_y):
     mag = np.sqrt(np.add(gradient_x**2, gradient_y**2))
     return mag
 
-#autocorrelation matrix 
+
+# autocorrelation matrix
 def autocorrelation_matrix(gradient_x_squared, gradient_x_y, gradient_y_squared, current_x, current_y, window_size):
     # weight is 1 because it is in the window
     weight = 1
     value = window_size // 2
     M = 0
-    #for each pixel the matrix is computed and stored
+    # for each pixel the matrix is computed and stored
     for i in range(current_x - value, current_x + value):
         for j in range(current_y - value, current_y + value):
             M = M + weight * np.matrix([[gradient_x_squared[i, j], gradient_x_y[i, j]], [gradient_x_y[i, j],
                                                                                          gradient_y_squared[i, j]]])
-
     return M 
 
 
 def cornerness(M):
 
-    if (det(M) == 0 or trace(M) == 0):
+    if np.linalg.det(M) == 0 or np.trace(M) == 0:
         return 0
-
-    return det(M) / trace(M)
+    else:
+        return np.linalg.det(M) / np.trace(M)
 
 
 def roundness(M):
 
-    if det(M) == 0 or trace(M) == 0:
+    if np.linalg.det(M) == 0 or np.trace(M) == 0:
         return 0
-
-    return 4 * det(M) / (trace(M) * trace(M))
-
-
-def det(M):
-    
-    m_1 = M[0, 0]
-    m_2 = M[0, 1]
-    m_3 = M[1, 0]
-    m_4 = M[1, 1]
-
-    return m_1 * m_4 - m_2 * m_3
+    else:
+        return (4 * np.linalg.det(M)) / (np.trace(M)**2)
 
 
-def trace(M):
-
-    m_1 = M[0, 0]
-    m_4 = M[1, 1]
-
-    return m_1 + m_4
-
-#binary mask with threshold
+# binary mask with threshold
 def derive_binary_mask(img_name, image_size, W, Q, threshold_w, threshold_q):
 
     binary_mask = np.zeros((image_size[0], image_size[1]))
 
     for i in range(0, image_size[0] - 1):
         for j in range(0, image_size[1] - 1):
-            if (W[i][j] > threshold_w):
-                if (Q[i][j] > threshold_q):
+            if W[i][j] > threshold_w:
+                if Q[i][j] > threshold_q:
                     binary_mask[i][j] = 1
 
     plt.figure()
     plt.imshow(binary_mask, cmap='gray')
     plt.axis('off')
-    # plt.show()
     plt.savefig("Binary_Mask-" + img_name + ".jpg")
 
     return binary_mask
@@ -117,9 +102,9 @@ def foerstner(img_name, gradient_x, gradient_y, image_size):
     print("foerstner...")
     print("don't worry, it lasts some time...")
 
-    gradient_x_squared = gradient_x * gradient_x
+    gradient_x_squared = gradient_x**2
     gradient_x_y = gradient_x * gradient_y
-    gradient_y_squared = gradient_y * gradient_y
+    gradient_y_squared = gradient_y**2
 
     #thresholds according to task
     threshold_w = 0.004
@@ -131,8 +116,8 @@ def foerstner(img_name, gradient_x, gradient_y, image_size):
     Q = np.zeros((image_size[0], image_size[1]))
 
     # for each pixel in the image
-    for current_x in range(0, image_size[0]-1):
-        for current_y in range(0, image_size[1]-1):
+    for current_x in range(0, image_size[0] - 1):
+        for current_y in range(0, image_size[1] - 1):
 
             M = autocorrelation_matrix(gradient_x_squared, gradient_x_y, gradient_y_squared, current_x, current_y, window_size)
 
@@ -141,7 +126,6 @@ def foerstner(img_name, gradient_x, gradient_y, image_size):
 
             q = roundness(M)
             Q[current_x, current_y] = q
-
     binary_mask = derive_binary_mask(img_name, image_size, W, Q, threshold_w, threshold_q)
 
     # plot images for cornerness and roundness with jet color mapping
@@ -227,7 +211,7 @@ def plot_task_1(orig_img, gradient_x, gradient_y, mag, img_name):
     plt.savefig("GoG-Filtering-" + img_name + ".jpg")
 
 
-def task_2(orig_img, img_name, norm, gradient_x, gradient_y):
+def task_2(img_name, norm, gradient_x, gradient_y):
 
     print("\n---Task 2--- " + img_name + "\n")
     image_size = [int(norm.shape[0]), int(norm.shape[1])]
@@ -242,6 +226,7 @@ def get_norm_input(img_path):
     img = load_image(img_path)
     img_name = img_path.split('.')[0]
     orig_img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+    orig_img = cv2.cvtColor(orig_img, cv2.COLOR_BGR2RGB)
 
     plt.figure()
     plt.imshow(orig_img)
@@ -249,7 +234,6 @@ def get_norm_input(img_path):
     # plt.show()
     plt.savefig("Origin-" + img_name + ".jpg")
 
-    orig_img = cv2.cvtColor(orig_img, cv2.COLOR_BGR2RGB)
     norm = np.zeros(img.shape, dtype=np.float32)
     # normalize the input image
     norm = np.float32(cv2.normalize(img, norm, 0.0, 1.0, cv2.NORM_MINMAX))
@@ -261,13 +245,13 @@ def main():
 
     img_path_1 = 'ampelmaennchen.png'
     img_path_2 = 'flower.jpg'
-    img_path_3 = 'berlin.jpg'
+    # img_path_3 = 'berlin.jpg'
 
     # -------------------- input --------------------------
 
     orig_img_1, norm_1, img_name_1 = get_norm_input(img_path_1)
     orig_img_2, norm_2, img_name_2 = get_norm_input(img_path_2)
-    orig_img_3, norm_3, img_name_3 = get_norm_input(img_path_3)
+    # orig_img_3, norm_3, img_name_3 = get_norm_input(img_path_3)
 
     # -------------------- Task 1 -------------------------
 
@@ -277,14 +261,14 @@ def main():
     gradient_x_2, gradient_y_2, mag_2 = task_1(norm_2, img_name_2)
     plot_task_1(orig_img_2, gradient_x_2, gradient_y_1, mag_2, img_name_2)
     
-    gradient_x_3, gradient_y_3, mag_3 = task_1(norm_3, img_name_3)
-    plot_task_1(orig_img_3, gradient_x_3, gradient_y_3, mag_3,  img_name_3)
+    # gradient_x_3, gradient_y_3, mag_3 = task_1(norm_3, img_name_3)
+    # plot_task_1(orig_img_3, gradient_x_3, gradient_y_3, mag_3,  img_name_3)
 
     # -------------------- Task 2 -------------------------
 
-    task_2(orig_img_1, img_name_1, norm_1, gradient_x_1, gradient_y_1)
-    task_2(orig_img_2, img_name_2, norm_2, gradient_x_2, gradient_y_2)
-    task_2(orig_img_3, img_name_3, norm_3, gradient_x_3, gradient_y_3)
+    task_2(img_name_1, norm_1, gradient_x_1, gradient_y_1)
+    # task_2(img_name_2, norm_2, gradient_x_2, gradient_y_2)
+    # task_2(img_name_3, norm_3, gradient_x_3, gradient_y_3)
 
 
 if __name__ == '__main__':
