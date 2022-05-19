9feee233177430cc8f5bd3d36c6483ac24eac31d
xiqe4652 <nicola.lea.libera@uni-weimar.de>
2022-05-19 15:29:28 +0200

    finished task1 b

--- assignment3/assignment3.py
+++ assignment3/assignment3.py
@@ -28,9 +28,29 @@ def load_image(path):
     return norm
 
 
+def add_gaussian_noise(img):
+    mean = 0
+    variance = 0.01
+    sigma = sqrt(variance)
+    gaussian = np.random.normal(mean, sigma, img.shape)
+
+    return img + gaussian
+
+
+def plot_image(img, img_name):
+
+    plt.figure()
+    plt.title("img_name")
+    plt.imshow(img, cmap='gray')
+    plt.savefig(img_name + ".jpg")
+
+
 def main():
     img_path_task1 = 'taskA.png'
     task_1_img = load_image(img_path_task1)
+    plot_image(task_1_img, "normal")
+    task_1_img_noise = add_gaussian_noise(task_1_img)
+    plot_image(task_1_img_noise, "gaussian noise")
 
 
 if __name__ == '__main__':
