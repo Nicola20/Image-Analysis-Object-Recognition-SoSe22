83dda7cf488456b217d17379b05c49294763c361
xiqe4652 <nicola.lea.libera@uni-weimar.de>
2022-05-20 14:17:23 +0200

    added computation for gaussian kernel

--- assignment3/assignment3.py
+++ assignment3/assignment3.py
@@ -9,6 +9,7 @@ Description: Source code to Assignment 3 of the course Image Analysis and Object
 
 import numpy as np
 import cv2
+import math
 from matplotlib import pyplot as plt
 
 
@@ -31,10 +32,29 @@ def load_image(path):
 def add_gaussian_noise(img):
     mean = 0
     variance = 0.01
-    sigma = sqrt(variance)
+    sigma = math.sqrt(variance)
     gaussian = np.random.normal(mean, sigma, img.shape)
 
     return img + gaussian
+
+
+# computation of the kernel
+def create_gaussian_kernel(sigma):
+    # compute the radius and size of the kernel - maybe change this according to the result
+    radius = int(np.ceil(3 * sigma))
+    kernel_size = 2 * radius + 1
+
+    # initialize gaussian kernel
+    g = np.zeros((kernel_size, kernel_size), dtype=np.float32)
+
+    # math formula applied and values stored in the matrix
+    for y in range(-radius, radius + 1):
+        for x in range(-radius, radius + 1):
+            tmp1 = - (1 / (2 * np.pi * sigma ** 4))
+            tmp2 = - ((x ** 2 + y ** 2) / 2 * sigma ** 2)
+            g[y+radius, x+radius] = tmp1 * np.exp(tmp2)
+
+    return g
 
 
 def plot_image(img, img_name):
@@ -48,9 +68,10 @@ def plot_image(img, img_name):
 def main():
     img_path_task1 = 'taskA.png'
     task_1_img = load_image(img_path_task1)
-    plot_image(task_1_img, "normal")
+    #plot_image(task_1_img, "normal")
     task_1_img_noise = add_gaussian_noise(task_1_img)
-    plot_image(task_1_img_noise, "gaussian noise")
+    #plot_image(task_1_img_noise, "gaussian noise")
+    gaussian_kernel = create_gaussian_kernel(0.5)
 
 
 if __name__ == '__main__':
