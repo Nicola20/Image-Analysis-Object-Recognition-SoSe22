5a197d78f3bff914087e743cb1dd6409f8056fbd
flor2468 <jd20001@freenet.de>
2022-06-22 03:14:23 +0200

    added approach for task 2c + d and added complete task 1

--- assignment4/assignment4.py
+++ assignment4/assignment4.py
@@ -17,11 +17,13 @@ from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm, colors
 from scipy import ndimage
-# from skimage.segmentation import watershed
-# from skimage.feature import peak_local_max
 
 
+# max number of iterations for the k means clustering algorithm in case of an infity loop
 MAX_ITER = 300
+# number of cluster centroids for the k means clustering algorithm
+# testing with k = 3, 5, 10, 15, 20
+K = 20
 
 
 def load_image(path):
@@ -42,42 +44,31 @@ def load_gray_image(path):
     return norm
 
 
-
-def rgb_feature_space(img, color_centroids):
+def rgb_feature_space(img, color_space, output_name):
     # code from: https://realpython.com/python-opencv-color-spaces/
     r, g, b = cv2.split(img)
-    print(color_centroids)
-    pixel_colors = img.reshape((np.shape(img)[0] * np.shape(color_centroids)[1], 3))
-    norm = colors.Normalize()
-    norm.autoscale(pixel_colors)
-    pixel_colors = norm(pixel_colors).tolist()
+    colors = color_space.reshape((np.shape(color_space)[0] * np.shape(color_space)[1], 3))
 
     fig = plt.figure()
     axis = fig.add_subplot(1, 1, 1, projection="3d")
-    plt.title("3D RGB Feature Space")
-    axis.scatter(r.flatten(), g.flatten(), b.flatten(), facecolors=pixel_colors, marker=".")
+    plt.title("3D RGB Feature Space with k = " + str(K))
+    axis.scatter(r.flatten(), g.flatten(), b.flatten(), facecolors=colors,  marker=".")
     axis.set_xlabel("Red")
     axis.set_ylabel("Green")
     axis.set_zlabel("Blue")
-    plt.savefig("output-images/rgb-feature-space-clustering_result.jpg")
+    plt.savefig("output-images/" + output_name)
 
 
 def k_means_clustering(img, k):
     iter_count = 0
     loop = True
     feature_space = img.reshape((img.shape[0] * img.shape[1], 3))
-    print("shape img: " + str(img.shape))
-    print("featurespace shape: " + str(feature_space.shape))
-    #print(feature_space)
-    previous_centroids = np.random.rand(k, 3) # check here for uniformly distributed rand
+    color_space = copy.deepcopy(img.reshape((img.shape[0] * img.shape[1], 3)))
+    # create list of random cluster centroids
+    previous_centroids = np.random.rand(k, 3)
     current_centroids = copy.deepcopy(previous_centroids)
-    print("starting previous")
-    print(previous_centroids)
-    print("starting current")
-    print(current_centroids)
 
     while loop and iter_count <= MAX_ITER:
-        #print("iteration round: " + str(iter_count))
         # these distances have the structure: in each row we have the distance for the point with
         # [centroid1, centroid2, centroid3, ..., centroidk]
         distances = cdist(feature_space, current_centroids, 'euclidean')
@@ -90,15 +81,20 @@ def k_means_clustering(img, k):
             if new_centroid.all() != 0:
                 current_centroids[c] = new_centroid
 
-        if np.array_equal(previous_centroids, current_centroids):
-            print("same")
+        # end the algorithm if the cluster centroids do not change anymore or the max number of iterations is reached
+        if np.array_equal(previous_centroids, current_centroids) or iter_count == MAX_ITER:
+            print("Found cluster centroids")
             # assign all points to the nearest centroid
+            color_map_viridis = cm.get_cmap('jet', k)
             for c in range(len(current_centroids)):
                 # get the indices of the rows with  the smallest distance for c
                 mins = np.where(distances[:, c] == distances.min(axis=1))[0]
-                # maybe not the best idea to show the categorical data
+
+                # assign the colors of the pixels to the nearest cluster centroid
                 feature_space[mins] = copy.deepcopy(current_centroids[c])
 
+                # assign a color out of the color map to the cluster centroid
+                color_space[mins] = color_map_viridis(c)[:3]
             # stop the iteration
             loop = False
         else:
@@ -106,10 +102,8 @@ def k_means_clustering(img, k):
 
         iter_count += 1
     image_space = np.reshape(feature_space, (img.shape[0], img.shape[1], 3))
-
-    # plot the resulting image from the clustering
-    plot_image(image_space, "Clustered image with k =" + str(k), "cluster-img-k-" + str(k))
-    rgb_feature_space(img, current_centroids)
+    color_space = np.reshape(color_space, (img.shape[0], img.shape[1], 3))
+    return image_space, color_space
 
 
 def centroid(points, data):
@@ -118,9 +112,10 @@ def centroid(points, data):
     if num_of_points != 0:
         # get all points from the feature space that have a minimal distance to one of the current centroids
         image_pixel = np.take(data, points, axis=0)
-        # print(image_pixel[0])
+        # get the sum of the r, g, b columns
         column_sum = image_pixel.sum(axis=0)
 
+        # compute the new centroids
         centroid_x = column_sum[0] / num_of_points
         centroid_y = column_sum[1] / num_of_points
         centroid_z = column_sum[2] / num_of_points
@@ -134,136 +129,17 @@ def centroid(points, data):
 def plot_image(img, title, img_name):
     plt.figure()
     plt.title(title)
-    plt.imshow(img, cmap='nipy_spectral')
+    plt.imshow(img)
     plt.axis('off')
-    plt.savefig(img_name + ".jpg")
+    plt.savefig("output-images/" + img_name)
 
 
 def gradient_magnitude(img):
     return ndimage.gaussian_gradient_magnitude(img, sigma=0.5)
      
 
-"""
-def neighborhood(img, x, y):
-
-    neighborhood_region_numbers = {}
-    
-    for i in range(-1, 2):
-        for j in range(-1, 2):
-            if (i == 0 and j == 0):
-                continue
-            if (x + i < 0 or y + j < 0):
-                continue
-            if (x + i >= img.shape[0] or y + j >= img.shape[1]):
-                continue
-            if (neighborhood_region_numbers.get(img[x + i][y + j]) == None):
-                neighborhood_region_numbers[img[x + i][y + j]] = 1
-            else:
-                neighborhood_region_numbers[img[x + i][y + j]] += 1
-
-    if (neighborhood_region_numbers.get(0) != None):
-        del neighborhood_region_numbers[0]
-
-    keys = list(neighborhood_region_numbers)
-
-    keys.sort()
-
-    if (keys[0] == -1):
-        if (len(keys) == 1):
-            return -1
-        elif (len(keys) == 2):
-            return keys[1]
-        else:
-            return 0
-    else:
-        if(len(keys) == 1):
-            return keys[0]
-        else:
-            return 0
-
-
-def watershed_algorithm(img):
-
-    intensity_list = []
-
-    for x in range(img.shape[0]):
-        for y in range(img.shape[1]):
-            intensity_list.append((img[x][y], (x, y)))
-
-    # intensity_list.sort()
-
-    segmented_img = np.full(img.shape, -1, dtype=int)
-
-    region_number = 0
-
-    for i in range(len(intensity_list)):
-   
-        x = intensity_list[i][1][0]
-        y = intensity_list[i][1][1]
-
-        region_status = neighborhood(segmented_img, x, y)
-
-        if (region_status == -1):
-            region_number += 1
-            segmented_img[x][y] = region_number
-        elif (region_status == 0):
-            segmented_img[x][y] = 0
-        else:
-            segmented_img[x][y] = region_status
-
-    return segmented_img
-"""
-
-def watershed_built_in(path):
-
-    img = cv2.imread(path)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-    kernel = np.ones((3,3), np.uint8)
-    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2)
-
-    sure_bg = cv2.dilate(opening, kernel, iterations=5)
-    dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
-    ret, sure_fg = cv2.threshold(dist_transform, 0.7*dist_transform.max(),255,0)
-
-    sure_fg = np.uint8(sure_fg)
-    unknown = cv2.subtract(sure_bg, sure_fg)
-    ret, markers = cv2.connectedComponents(sure_fg)
-
-    markers = markers + 1
-    markers[unknown==255] = 0
-    markers = cv2.watershed(img, markers)
-    img[markers == -1] = [255,0,0]
-
-    return markers
-
-
-# def watershed_built_in_2(img):
-
-#     distance = ndimage.distance_transform_edt(img)
-#     coords = peak_local_max(distance, footprint=np.ones((3,3)), labels=img)
-#     mask = np.zeros(distance.shape, dtype=bool)
-#     mask[tuple(coords.T)] = True
-#     markers, _ = ndimage.label(maks)
-#     labels = watershed(-distance, markers, mask=img)
-
-#     fig, axes = plt.subplots(ncols=3, figsize=(9,3), sharex=True, sharey=True)
-#     ax = axes.ravel()
-
-#     ax[0].imshow(img, cmap=plt.cm.gray)
-#     ax[0].set_title('Overlapping Area')
-#     ax[1].imshow(-distance, cmap=plt.cm.gray)
-#     ax[1].set_title('Distances')
-#     ax[2].imshow(labels, cmap=plt.cm.nipy_spectral)
-#     ax[2].set_title('Separated Objects')
-
-#     for a in ax:
-#         a.set_axis_off()
-
-#     fig.tight_layout()
-#     plt.show()
-
 def plot_seed_points(img, seed_points):
+
     plt.figure()
     plt.title("Seed Points")
     plt.axis('off')
@@ -272,41 +148,129 @@ def plot_seed_points(img, seed_points):
         plt.plot(point[0], point[1], marker='o', color="blue")
 
     plt.imshow(img)
-    plt.savefig("task_2_seed_points" + ".jpg")
+    plt.savefig("output-images/task_2_seed_points" + ".jpg")
 
 
-def watershed(gradient_img):
+def non_functional_watershed_segmentation(gradient_img):
 
+    # This function is another approach of implementing the watershed segmentation algorithm.
+    # Unfortunately this function cannot be executed, because it ends in an endless loop
+    # because of the condition for the while loop.
+    # It should break if the seed_points list is empty, but it does not, because of an error,
+    # which unfortunately could not be found.
+    # This function uses many implemented functions, which work fine. Because of the described 
+    # error, only the neighboorhood() function can be used in the watershed_segmentation()
+    # function.
+
+    counter = 0
     seed_points = [[40, 110], [140, 90], [5, 60], [130, 20], [70, 30], [100, 130]]
-
-    measure_of_similarity = 0.1
-
-    # test_neighbor = neighborhood(gradient_img, seed_points[0])
-    # test_neighbor = neighborhood(gradient_img, (0, 0))
-    # print(test_neighbor)
-    # test_colors = get_neighbor_color_values(gradient_img, test_neighbor)
-    # print(test_colors)
+    measure_of_similarity = 0.3
 
     region_array = [[] for x in range(len(seed_points))]
-    
     for i in range(len(seed_points)):
-        
-        current_point = seed_points[i]
-        current_point_color = get_point_color_value(gradient_img, current_point)
-        neighbors = neighborhood(current_point)
-        neighbor_colors = get_neighbor_color_values(gradient_img, neighbors)
-        
-        for j in range(len(neighbors)):
-            if (np.abs(neighbor_colors[j] - current_point_color) <= measure_of_similarity):
-                region_array[i].append(neighbors[j])
-    
+        region_array[i].append(seed_points[i])
+
+    visited_points = np.zeros(shape=(gradient_img.shape), dtype=np.uint8)
+
+    for i in range(len(region_array)):
+
+        print("region " + str(i))
+        counter = 0
+        starting_seed_points = [seed_points[i]]
+
+        while (len(seed_points) > 0):
+            counter, starting_seed_points, visited_points, region_array[i] = do_watershed_segmentation(counter, gradient_img, starting_seed_points, visited_points, region_array[i], measure_of_similarity)
+
     for l in range(len(region_array)):
         print(len(region_array[l]))
 
     return region_array
 
 
+def do_watershed_segmentation(counter, gradient_img, seed_points, visited_points, region_array, measure_of_similarity):
+
+    counter += 1
+    print(counter)
+    new_seed_points = []
+
+    for i in range(len(seed_points)):
+
+        current_point = seed_points[i]
+        current_point_color = get_point_color_value(gradient_img, current_point)
+        neighbors = neighborhood(gradient_img, current_point)
+        try:
+            neighbor_colors = get_neighbor_color_values(gradient_img, neighbors)
+        except:
+            break
+        
+        for j in range(len(neighbors)):
+            if (not visited_points[neighbors[j][0]][neighbors[j][1]].all()):
+                visited_points[neighbors[j][0]][neighbors[j][1]] = 1
+
+                if (np.abs(neighbor_colors[j] - current_point_color) <= measure_of_similarity):
+                    new_seed_points.append(neighbors[j])
+                    region_array.append(neighbors[j])
+
+    return counter, new_seed_points, visited_points, region_array
+
+
+def watershed_segmentation(gradient_img, seed_points):
+
+    # inspired by https://pythonmana.com/2021/12/20211208035633797D.html 
+
+    measure_of_similarity = 0.01
+    segmented_array = np.zeros(shape=(gradient_img.shape), dtype=np.uint8)
+    visited_points = np.zeros(shape=(gradient_img.shape), dtype=np.uint8)
+
+    max_x = gradient_img.shape[0]
+    max_y = gradient_img.shape[1]
+
+    for point in seed_points:
+        x = point[0]
+        y = point[1]
+
+        segmented_array[y][x] = 255
+
+    while (len(seed_points) > 0):
+        point = seed_points.pop(0)
+        x = point[0]
+        y = point[1]
+
+        visited_points[y][x] = 1
+        neighbors = neighborhood(gradient_img, point)
+
+        for n in neighbors:
+            current_x = n[0]
+            current_y = n[1]
+
+            if (current_x < 0 or current_y < 0 or current_x >= max_x or current_y >= max_y):
+                continue
+
+            if (not visited_points[current_y][current_x].all()) and (np.abs(gradient_img[current_y][current_x][0] - gradient_img[y][x][0]) <= measure_of_similarity) :
+                segmented_array[current_y][current_x] = 255
+                visited_points[current_y][current_x] = 1
+                seed_points.append((current_x, current_y))
+
+    return segmented_array
+        
+
+def put_segmentation_onto_original_image(original_img, segmented_array):
+    
+    max_x = original_img.shape[0]
+    max_y = original_img.shape[1]
+    
+    for x in range(max_x):
+        for y in range(max_y):
+            if segmented_array[x][y][0] != 255:
+                original_img[x][y][0] = 0
+                original_img[x][y][1] = 0
+                original_img[x][y][2] = 0
+    
+    return original_img
+
+
 def plot_regions(region_array):
+
     plt.figure()
     plt.title("Regions")
     plt.axis('off')
@@ -317,20 +281,28 @@ def plot_regions(region_array):
         for point in region_array[i]:
             plt.plot(point[0], point[1], marker='o', markersize=3, color=colors[i])
 
-    plt.savefig("task_2_regions" + ".jpg")
+    plt.savefig("output-images/task_2_regions" + ".jpg")
 
     
-def neighborhood(point):
+def neighborhood(gradient_img, point):
     
     neighbors = []
-
+    max_x = gradient_img.shape[0]
+    max_y = gradient_img.shape[1]
     x = point[0]
     y = point[1]
+
+    if (x <= 0 or y <= 0 or x >= max_x or y >= max_y):
+        return neighbors
 
     neighbors.append((x + 1, y))
     neighbors.append((x - 1, y))
     neighbors.append((x, y + 1))
     neighbors.append((x, y - 1))
+    neighbors.append((x + 1, y + 1))
+    neighbors.append((x + 1, y - 1))
+    neighbors.append((x - 1, y + 1))
+    neighbors.append((x - 1, y - 1))
 
     return neighbors
 
@@ -348,26 +320,47 @@ def get_neighbor_color_values(array, neighbors):
 
     neighbor_colors = []
 
-    for n in range(len(neighbors)):
-        x = neighbors[n][0]
-        y = neighbors[n][1]
-        neighbor_colors.append(array[x][y][0])
+    if (len(neighbors) > 0):
+        for n in range(len(neighbors)):
+            x = neighbors[n][0]
+            y = neighbors[n][1]
+            neighbor_colors.append(array[x][y][0])
 
     return neighbor_colors
 
 
 def main():
-    # ------------------------------------- TASK 1 --------------------------------------------------
-    # print('task 1\n')
 
-    # input_ex5_1_img = load_image('input-images/inputEx5_1.jpg')
-    # #rgb_feature_space(input_ex5_1_img)
-    # # compute the k means clustering algorithm parameters = img, k
-    # k_means_clustering(input_ex5_1_img, 5)
+    # ------------------------------------- TASK 1 --------------------------------------------------
+
+    print('task 1\n')
+    input_ex5_1_img = load_image('input-images/inputEx5_1.jpg')
+    # compute the k means clustering algorithm parameters = img, k
+    original_image_ex5_1 = copy.deepcopy(input_ex5_1_img)
+    img_space, color_space = k_means_clustering(input_ex5_1_img, K)
+
+    # plot the resulting image and feature space from the clustering
+    rgb_feature_space(original_image_ex5_1, img_space, "feature space ex5_1 k " + str(K) + ".jpg")
+    plot_image(img_space, "Clustered image with k =" + str(K), "cluster ex5_1 k-" + str(K))
+    # plot the resulting image and feature space from the clustering
+    rgb_feature_space(original_image_ex5_1, color_space, "feature space ex5_1 k " + str(K) + " color mapped.jpg")
+    plot_image(color_space, "Clustered image with k =" + str(K), "cluster ex5_1 k-" + str(K) + " color mapped.jpg")
+
+    input_ex5_2_img = load_image('input-images/inputEx5_2.jpg')
+    original_image_ex5_2 = copy.deepcopy(input_ex5_2_img)
+    img_space, color_space = k_means_clustering(input_ex5_2_img, K)
+
+    # plot the resulting image and feature space from the clustering
+    rgb_feature_space(original_image_ex5_2, img_space, "feature space ex5_2 k" + str(K) + ".jpg")
+    plot_image(img_space, "Clustered image with k =" + str(K), "cluster ex5_2 k-" + str(K))
+    # plot the resulting image and feature space from the clustering
+    rgb_feature_space(original_image_ex5_2, color_space, "feature space ex5_2 k" + str(K) + " color mapped.jpg")
+    plot_image(color_space, "Clustered image with k =" + str(K), "cluster ex5_2 k-" + str(K) + " color mapped.jpg")
 
     # ------------------------------------- TASK 2 --------------------------------------------------
+
     print('task 2')
-    print('task a\n')
+    print('task a')
 
     task_2_img = load_gray_image("input-images/inputEx5_2.jpg")
     plot_image(task_2_img, 'Grayscale', 'task_2_grayscale')
@@ -375,17 +368,20 @@ def main():
     task_2_mag = gradient_magnitude(task_2_img)
     plot_image(task_2_mag, 'Gradient Magnitude', 'task_2_magnitude')
 
-    # task_2_segmented_img = watershed_built_in("input-images/inputEx5_2.jpg")
-    # plot_image(task_2_segmented_img, 'Watershed Segmentation Built-In', 'task_2_watershed_built_in')
-
     print('task b')
 
-    # seed_points = [[40, 110], [140, 90], [5, 60], [130, 20], [70, 30], [100, 130]]
-    # plot_seed_points(task_2_mag, seed_points)
+    seed_points = [[40, 110], [140, 90], [5, 60], [130, 20], [70, 30], [100, 130]]
+    plot_seed_points(task_2_mag, seed_points)
 
-    print('task c')
-    task_2_region_array = watershed(task_2_mag)
-    plot_regions(task_2_region_array)
+    print('task c + d')
+
+    task_2_original_img = load_image('input-images/inputEx5_2.jpg')
+
+    task_2_watershed_array = watershed_segmentation(task_2_mag, seed_points)
+    plot_image(task_2_watershed_array, 'Watershed', 'task_2_watershed_segementation')
+
+    task_2_overlap_array = put_segmentation_onto_original_image(task_2_original_img, task_2_watershed_array)
+    plot_image(task_2_overlap_array, 'Watershed Overlap', 'task_2_watershed_segmentation_overlap')
 
 
 if __name__ == '__main__':
