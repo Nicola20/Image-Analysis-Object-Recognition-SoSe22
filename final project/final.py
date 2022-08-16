4667268b069f81fb25d43877ddbdba51d6f886e8
Philipp Tornow <philipp_oeffentlich@hotmail.de>
2022-08-16 16:22:43 +0200

    finalized ex1

--- final project/final.py
+++ final project/final.py
@@ -7,7 +7,11 @@ Description: Source code to the final project of the course Image Analysis and O
 --------------------------------------------------------------------------------------------
 """
 
+from cmath import sqrt
+from configparser import Interpolation
 from textwrap import fill
+from tkinter import Scale
+from turtle import shape
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
@@ -24,21 +28,20 @@ def load_gray_image(path):
     return None
 
 
-
-
 def plot_gray_image(img, title, img_name, boundingBoxOrigin = None, boundingBoxDims = None):
-
     plt.figure()
     plt.title(title)
     plt.imshow(img, cmap='gray')
     plt.axis('off')
-    #plt.gca().invert_yaxis()
     plt.gca().add_patch(Rectangle(boundingBoxOrigin, boundingBoxDims[0], boundingBoxDims[1], fill=False, edgecolor='r'))
     plt.show()
     plt.savefig(img_name)
 
 
-def template_matching(img, template, stride=1):
+def template_matching(img, template, scale=1, stride=1):
+
+    pyramid_scale = (int(img.shape[1]*scale), int(img.shape[0]*scale))
+    img = cv2.resize(img, pyramid_scale, interpolation = cv2.INTER_LINEAR)
 
     # if template is even then add padding so that we get an uneven kernel size
     if template.shape[0] % 2 == 0:
@@ -49,11 +52,11 @@ def template_matching(img, template, stride=1):
     padding_y = int(np.floor(template.shape[0] / 2))
     padding_x = int(np.floor(template.shape[1] / 2))
     padded_img = np.pad(img, ((padding_y, padding_y), (padding_x, padding_x)), constant_values=1)
-    filtered_image = normalized_cross_correlation(padded_img, template, (padding_x,padding_y), stride)
+    filtered_image = normalized_cross_correlation(padded_img, template, (padding_y,padding_x), stride)
     return filtered_image, padding_x, padding_y
 
 
-def normalized_cross_correlation(paddedImage, template, padding, stride=1):
+def cross_correlation(paddedImage, template, padding, stride=1):
     mean_template_patch = np.mean(template)
     filtered_image = np.zeros((paddedImage.shape[0]-2*padding[0], paddedImage.shape[1]-2*padding[1]))
 
@@ -64,14 +67,20 @@ def normalized_cross_correlation(paddedImage, template, padding, stride=1):
     return filtered_image
 
 def normalized_cross_correlation(paddedImage, template, padding, stride=1):
+
     mean_template_patch = np.mean(template)
     filtered_image = np.zeros((paddedImage.shape[0]-2*padding[0], paddedImage.shape[1]-2*padding[1]))
+    print(filtered_image.shape)
 
     for m in range(filtered_image.shape[0]):
         for n in range(filtered_image.shape[1]):
-            filtered_image[m,n] = ((template - mean_template_patch) * paddedImage[m:m+template.shape[0],n:n+template.shape[1]]).sum()
+            sigma_ab = (1 / (template.shape[0]*template.shape[1])) * (template * paddedImage[m:m+template.shape[0],n:n+template.shape[1]]).sum() - mean_template_patch * np.mean(paddedImage[m:m+template.shape[0],n:n+template.shape[1]])
+            sigma_a = (1 / (template.shape[0]*template.shape[1])) * (template * template).sum() - (mean_template_patch*mean_template_patch)
+            sigma_b = (1 / (template.shape[0]*template.shape[1])) * (paddedImage[m:m+template.shape[0],n:n+template.shape[1]] * paddedImage[m:m+template.shape[0],n:n+template.shape[1]]).sum() - (np.mean(paddedImage[m:m+template.shape[0],n:n+template.shape[1]])**2)
+            filtered_image[m,n] = sigma_ab/(sqrt((sigma_a*sigma_b)))
 
     return filtered_image
+
 
 def main():
     # ------------------------------------- TASK 1 --------------------------------------------------
@@ -94,6 +103,8 @@ def main():
 
     templates = [temp1, temp2, temp3, temp4, temp5, temp6]
     images = [img1, img2, img3, img4, img5, img6]
+    scales = [1.0, 0.1,0.25, 0.5,0.75, 0.9]
+    scales = [1.0]
 
     for t in templates:
         if t is None:
@@ -105,17 +116,28 @@ def main():
             print("Abort")
             return -1
 
-    filtered_image, padding_x, padding_y = template_matching(img1, temp1)
-    ncc_max_index = np.unravel_index(np.argmax(filtered_image, axis=None), filtered_image.shape)
-    print("max index: " + str(ncc_max_index))
-    print("max value: " + str(filtered_image[ncc_max_index]))
 
-    thresholding = np.vectorize(lambda v: 1 if v>0.5 else 0)
-    thresholded_image = thresholding(filtered_image)
-    #thresholded_image = cv2.threshold(filtered_image, )
+    for iidx,image in enumerate(images):
+        for tidx,template in enumerate(templates):
+            for sidx,scale in enumerate(scales):
+                filtered_image, padding_x, padding_y = template_matching(image, template, scale)
+                ncc_max_index = np.unravel_index(np.argmax(filtered_image, axis=None), filtered_image.shape)
+                #print("max index: " + str(ncc_max_index))
+                #print("max value: " + str(filtered_image[ncc_max_index]))
 
-    plot_gray_image(filtered_image, "TITLE", "out.jpg", (ncc_max_index[0]-padding_x, ncc_max_index[1]+padding_y), temp1.shape)
-    #plot_gray_image(thresholded_image, "TITLE", "out.jpg")
+                thresholding = np.vectorize(lambda v: 1 if v>0.5 else 0)
+                thresholded_image = thresholding(filtered_image)
+                #thresholded_image = cv2.threshold(filtered_image, )
+
+                plot_gray_image(image,
+                    "Image "+str(iidx+1)+" Temp "+str(tidx+1)+" Scale "+str(scale),
+                    "results/scale1/Image"+str(iidx+1)+"_temp"+str(tidx+1)+"_scale"+str(scale)+".jpg",
+                    (ncc_max_index[1]-padding_x, ncc_max_index[0]-padding_y),
+                    template.shape
+                )
+                #plot_gray_image(thresholded_image, "TITLE", "out.jpg")
+
+
 
 
     # ------------------------------------- TASK 2 --------------------------------------------------
